c90958bde130f22d3d59e3c5d7abd29dba2430d4
Alexander Smityuk <sanchosmit@gmail.com>
2018-05-06 22:27:30 +0300

    Add info charts

--- app/src/main/java/com/things/smartwateringapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/things/smartwateringapp/ui/home/HomeFragment.kt
@@ -1,7 +1,12 @@
 package com.things.smartwateringapp.ui.home
 
+import android.graphics.Color
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.view.View
+import android.view.animation.OvershootInterpolator
+import com.hookedonplay.decoviewlib.charts.SeriesItem
+import com.hookedonplay.decoviewlib.events.DecoEvent
 import com.things.smartwateringapp.App
 import com.things.smartwateringapp.R
 import com.things.smartwateringapp.di.home.HomeModule
@@ -9,14 +14,22 @@ import com.things.smartwateringapp.domain.entity.DataInfo
 import com.things.smartwateringapp.presentation.home.HomePresenter
 import com.things.smartwateringapp.presentation.home.HomeView
 import com.things.smartwateringapp.ui.BaseFragment
+import kotlinx.android.synthetic.main.fragment_home.*
+import java.util.*
 import javax.inject.Inject
 
-class HomeFragment : BaseFragment() , HomeView {
+
+class HomeFragment : BaseFragment(), HomeView {
 
     override val layoutRes: Int = R.layout.fragment_home
 
     @Inject
     lateinit var presenter: HomePresenter
+
+    companion object {
+        const val MAIN_TRACK_DURATION = 3000L
+        const val BACK_TRACK_DURATION = 500L
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +43,6 @@ class HomeFragment : BaseFragment() , HomeView {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         presenter.bindView(this)
         presenter.getDataInfo()
     }
@@ -38,13 +50,76 @@ class HomeFragment : BaseFragment() , HomeView {
     override fun onDestroyView() {
         super.onDestroyView()
         presenter.unbindView()
+        humidityChart.executeReset()
+        soilMoistureChart.executeReset()
     }
 
     override fun showInfo(info: DataInfo) {
+        humidityText.text = String.format(Locale.getDefault(), resources.getString(R.string.humidity_percentage), info.humidity)
+        soilMoistureText.text = String.format(Locale.getDefault(), resources.getString(R.string.soil_moisture_percentage), info.soilMoisture)
+        temperature.text = "${info.temperature}°C"
 
+        setupHumidityChart(info.humidity.toFloat())
+        setupSoilMoistureChart(info.soilMoisture.toFloat())
     }
 
     override fun showError(message: Int) {
 
+    }
+
+    private fun setupHumidityChart(humidity: Float) {
+
+        val backgroundTrack = SeriesItem.Builder(ContextCompat.getColor(context!!, R.color.materialGrey))
+                .setRange(0f, 100f, 100f)
+                .setInitialVisibility(false)
+                .build()
+
+        humidityChart.addEvent(DecoEvent.Builder(DecoEvent.EventType.EVENT_SHOW, true)
+                .setDuration(BACK_TRACK_DURATION)
+                .setIndex(humidityChart.addSeries(backgroundTrack)).build())
+
+        val mainTrack = SeriesItem.Builder(ContextCompat.getColor(context!!, R.color.materialYellow))
+                .setRange(0f, 100f, 0f)
+                .setInterpolator(OvershootInterpolator())
+                .setShowPointWhenEmpty(true)
+                .setCapRounded(true)
+                .setSpinDuration(MAIN_TRACK_DURATION)
+                .setChartStyle(SeriesItem.ChartStyle.STYLE_DONUT)
+                .build()
+
+        humidityChart.addEvent(DecoEvent.Builder(humidity).setIndex(humidityChart.addSeries(mainTrack)).build())
+    }
+
+    private fun setupSoilMoistureChart(soilMoisture: Float) {
+
+        val backgroundTrack = SeriesItem.Builder(ContextCompat.getColor(context!!, R.color.materialGrey))
+                .setRange(0f, 100f, 100f)
+                .setInitialVisibility(false)
+                .build()
+
+        soilMoistureChart.addEvent(DecoEvent.Builder(DecoEvent.EventType.EVENT_SHOW, true)
+                .setDuration(BACK_TRACK_DURATION)
+                .setIndex(soilMoistureChart.addSeries(backgroundTrack)).build())
+
+        val series = SeriesItem.Builder(ContextCompat.getColor(context!!, R.color.materialBlue))
+                .setRange(0f, 100f, 0f)
+                .setInterpolator(OvershootInterpolator())
+                .setShowPointWhenEmpty(true)
+                .setCapRounded(true)
+                .setSpinDuration(MAIN_TRACK_DURATION)
+                .setChartStyle(SeriesItem.ChartStyle.STYLE_DONUT)
+                .build()
+
+        soilMoistureChart.addEvent(DecoEvent.Builder(soilMoisture).setIndex(soilMoistureChart.addSeries(series)).build())
+
+        series.addArcSeriesItemListener(object : SeriesItem.SeriesItemListener {
+            override fun onSeriesItemAnimationProgress(percentComplete: Float, currentPosition: Float) {
+                val percentFilled = (currentPosition - series.minValue) / (series.maxValue - series.minValue)
+                soilMoisturePercentage.text = String.format("%.0f%%", percentFilled * 100f)
+            }
+
+            override fun onSeriesItemDisplayProgress(percentComplete: Float) {
+            }
+        })
     }
 }
